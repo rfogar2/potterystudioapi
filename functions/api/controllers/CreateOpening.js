fe8823d0d0df0ba5bdc09fdf9fc9b635ddaf4bc5
Ryan Fogarty <rfogar2@uic.edu>
2020-03-08 11:38:34 +0000

    Allow creating recurring openings

--- functions/api/controllers/CreateOpening.js
+++ functions/api/controllers/CreateOpening.js
@@ -1,26 +1,57 @@
+const moment = require("moment")
 const Firebase = require("../services/firebase")
 const validOpening = require("../validation/validOpening")
+const uuid = require("uuid")
 
 /*
 {
     start: Date,
     lengthSeconds: Int,
-    size: Int
+    size: Int,
+    recurrence?: {
+        type: only "WEEKLY" currently supported,
+        numberOfRecurrences: Int
+    }
 }
 */
 
 module.exports = (async (req, res) => {
     let body = JSON.parse(JSON.stringify(req.body))
-    const { start, lengthSeconds, size } = body
+    const { start, lengthSeconds, size, recurrence } = body
 
-    if (!validOpening(start, lengthSeconds, size )) {
+    if (!validOpening(start, lengthSeconds, size, null, recurrence)) {
         res.status(400).send("Invalid opening")
         return
     }
 
-    const opening = { start, lengthSeconds, size }
-    const ref = await Firebase.openings_store.add(opening)
-    opening["id"] = ref.id
+    const occurrences = recurrence ? recurrence.numberOfOccurrences : 1
+    const recurrenceId = uuid.v4()
+    const openings = []
 
-    res.status(201).send(opening)
+    for (i = 0; i < occurrences; i++) {
+        const durationUnit = recurrenceTypeToDurationUnit(recurrence ? recurrence.type : "")
+        const openingStart = moment(start).add(i, durationUnit)
+
+        const opening = { openingStart, lengthSeconds, size }
+        if (occurrences > 1) {
+            opening.recurrenceId = recurrenceId
+        }
+
+        // todo: use batching to avoid doing multiple writes
+        const ref = await Firebase.openings_store.add(opening)
+        opening["id"] = ref.id
+        openings.push(opening)
+    }
+
+    res.status(201).send(openings)
 })
+
+recurrenceTypeToDurationUnit = (type) => {
+    if (type === "DAILY") {
+        return "days"
+    } else if (type === "WEEKLY") {
+        return "weeks"
+    } else {
+        return "months"
+    }
+}
--- functions/api/validation/validOpening.js
+++ functions/api/validation/validOpening.js
@@ -1,7 +1,10 @@
 const moment = require("moment")
 
-module.exports = (start, lengthSeconds, size, usersBooked) => 
+module.exports = (start, lengthSeconds, size, usersBooked, recurrence) => 
     moment(start, moment.ISO_8601, true).isValid() &&
-    lengthSeconds > 0 && Number.isInteger(lengthSeconds) &&
-    size > 0 && Number.isInteger(size) &&
-    (!usersBooked || (usersBooked && usersBooked.every((userId) => typeof userId === "string")))
+    Number.isInteger(lengthSeconds) && lengthSeconds > 0 &&
+    Number.isInteger(size) && size > 0 &&
+    (!usersBooked || (usersBooked && usersBooked.every((userId) => typeof userId === "string"))) &&
+    (!recurrence || 
+        (recurrence.type && ["DAILY", "WEEKLY", "MONTHLY"].some(t => recurrence.type.toUpperCase() === t) &&
+        Number.isInteger(recurrence.numberOfOccurrences) && recurrence.numberOfOccurrences > 0))
