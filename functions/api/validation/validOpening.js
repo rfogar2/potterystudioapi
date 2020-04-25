fe75c2c487b39e197cac906eb7fdc2d8585131e0
Ryan Fogarty <ryan.fogarty@brighthr.com>
2020-04-25 12:56:00 +0100

    Allow size 0 openings

--- functions/api/validation/validOpening.js
+++ functions/api/validation/validOpening.js
@@ -3,7 +3,7 @@ const moment = require("moment")
 module.exports = (start, lengthSeconds, size, usersBooked, recurrence) => 
     moment(start, moment.ISO_8601, true).isValid() &&
     Number.isInteger(lengthSeconds) && lengthSeconds > 0 &&
-    Number.isInteger(size) && size > 0 &&
+    Number.isInteger(size) && size >= 0 &&
     (!usersBooked || (usersBooked && usersBooked.every((userId) => typeof userId === "string"))) &&
     (!recurrence || 
         (recurrence.type && ["DAILY", "WEEKLY", "MONTHLY"].some(t => recurrence.type.toUpperCase() === t) &&
