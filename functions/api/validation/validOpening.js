cbfbf9eac5314915f88effb6d74cc2566dd832fa
Ryan Fogarty <ryan.fogarty@brighthr.com>
2020-04-25 13:15:14 +0100

    Small cleanup

--- functions/api/validation/validOpening.js
+++ functions/api/validation/validOpening.js
@@ -4,7 +4,7 @@ module.exports = (start, lengthSeconds, size, usersBooked, recurrence) =>
     moment(start, moment.ISO_8601, true).isValid() &&
     Number.isInteger(lengthSeconds) && lengthSeconds > 0 &&
     Number.isInteger(size) && size >= 0 &&
-    (!usersBooked || (usersBooked && usersBooked.every((userId) => typeof userId === "string"))) &&
+    (!usersBooked || usersBooked.every((userId) => typeof userId === "string")) &&
     (!recurrence || 
         (recurrence.type && ["DAILY", "WEEKLY", "MONTHLY"].some(t => recurrence.type.toUpperCase() === t) &&
         Number.isInteger(recurrence.numberOfOccurrences) && recurrence.numberOfOccurrences > 0))
