4f750b4397a364d85aff726ac150340890ec4810
Ryan Fogarty <ryan.fogarty@brighthr.com>
2020-06-09 09:53:02 +0100

    cors fix

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,12 @@
 const functions = require('firebase-functions')
 const app = require('express')()
-const cors = require('cors')()
+const cors = require('cors')({origin: true})
 const bodyParser = require('body-parser')
 const router = require('./api/router')
 const auth = require("./api/services/auth");
 
-app.use(auth)
 app.use(cors)
+app.use(auth)
 app.use(bodyParser.json())
 router(app)
 
